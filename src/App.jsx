aef4c235fa358e5ba1609b9d44fc3ff1eb152e2f
rajalishikhe <nahidujjamanraj@gmail.com>
2025-05-09 19:52:18 +0600

    My basic setup and navbar section completet

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './Componets/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Navbar></Navbar>
+      
+      
+    </>
+  )
+}
+
+export default App
