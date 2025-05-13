4f86e9bc16e60a1929ce9b6ac60cbc6b440d18ce
rajalishikhe <nahidujjamanraj@gmail.com>
2025-05-13 10:38:00 +0600

    If any data not show image we can see the error image bye default

--- src/Componets/Card/Card.jsx
+++ src/Componets/Card/Card.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
-
+import img from "../../image/404 (1).jpg";
 const Card = ({blog}) => {
     console.log(blog)
     const{cover_image,created_at,description,title,id}=blog
     return (
         <div>
             <div>
-			<Link rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image} />
+			<Link to={`/blog/${id}`} rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
+				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image || img } />
 				<div className="p-6 space-y-2">
 					<h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
 					<span className="text-xs dark:text-gray-600">{created_at}</span>
