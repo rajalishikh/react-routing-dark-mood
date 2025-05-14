e52b059a13ee8bc3b614398202b60b34510ff0e6
rajalishikhe <nahidujjamanraj@gmail.com>
2025-05-14 09:51:00 +0600

    dynamicly we set conditional button

--- src/Componets/BlogDetails/BlogDetails.jsx
+++ src/Componets/BlogDetails/BlogDetails.jsx
@@ -1,9 +1,11 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const BlogDetails = () => {
     const loaderData=useLoaderData()
     console.log(loaderData)
     const{cover_image,created_at,description,title,id}=loaderData
+    const [tab,setTab]=useState(0)
     return (
         <div>
             <div className="max-w-2xl px-6 py-16 mx-auto space-y-12">
@@ -21,7 +23,28 @@ const BlogDetails = () => {
 		<div className="dark:text-gray-800">
 			<p>Insert the actual text content here...</p>
 		</div>
-	</article>
+   </article>
+{/* tab section dynamic start   */}
+<div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-center flex-nowrap dark:bg-gray-100 dark:text-gray-800">
+	<Link onClick={()=>setTab(0)} rel="noopener noreferrer" href="#" 
+    className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 
+        ${tab ===0 ?"border border-b-0 ":"border-b"} dark:text-gray-600 dark:border-gray-600 `}>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+			<path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+		</svg>
+		<span>Architecto</span>
+	</Link>
+	<Link onClick={()=>setTab(1)} rel="noopener noreferrer" href="#" className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 
+        ${tab ===1 ?"border border-b-0 ":"border-b"}dark:text-gray-600 dark:border-gray-600`} >
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+			<path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+			<path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+		</svg>
+		<span>Corrupti</span>
+	</Link>
+</div>
+{/* tab section dynamic end    */}
+
 	<div>
 		<div className="flex flex-wrap py-6 gap-2 border-t border-dashed dark:border-gray-600">
 			<a rel="noopener noreferrer" href="#" className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50">#MambaUI</a>
