ca50409f446f7b39b3d79803eb120c2b8993dc86
rajalishikhe <nahidujjamanraj@gmail.com>
2025-05-18 09:46:27 +0600

    Now  I am separate the routes

---
+++ src/MainRoutes/Routes.jsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Author from '../Componets/Author/Author';
+import BlogDetails from '../Componets/BlogDetails/BlogDetails.Jsx';
+import Blog from '../Componets/Blogs/Blog';
+import BookMarks from '../Componets/BookMarks/BookMarks';
+import Content from '../Componets/Content/Content';
+import Home from '../Componets/Home/Home';
+import MainLayOut from '../Componets/MainLayOut/MainLayOut';
+
+export  const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<MainLayOut></MainLayOut> ,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+
+      },
+      
+      {
+        path:"/Blogs",
+        element:<Blog></Blog>,
+        loader:()=>fetch('https://dev.to/api/articles?per_page=20?top=7')
+      },
+      {
+        path:`/blog/:id`,
+        element:<BlogDetails></BlogDetails>,
+        loader:({params})=>fetch(`https://dev.to/api/articles/${params.id}`),
+        children:[
+          {
+            index:true,
+            element:<Content></Content>
+          },
+          {
+            path:"author",
+            element:<Author></Author>
+            
+          }
+        ]
+      },
+      {
+        path:"/BookMarks",
+        element:<BookMarks></BookMarks>
+      }
+    ]
+  },
+]);
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,58 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import {
-  createBrowserRouter,
-  RouterProvider,
+  RouterProvider
 } from "react-router-dom";
 
-import Author from './Componets/Author/Author.jsx';
-import BlogDetails from './Componets/BlogDetails/BlogDetails.Jsx';
-import Blog from './Componets/Blogs/Blog.jsx';
-import BookMarks from './Componets/BookMarks/BookMarks.jsx';
-import Content from './Componets/Content/Content.jsx';
-import Home from './Componets/Home/Home.jsx';
-import MainLayOut from './Componets/MainLayOut/MainLayOut.jsx';
 import './index.css';
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<MainLayOut></MainLayOut> ,
-    children:[
-      {
-        path:"/",
-        element:<Home></Home>
+import { router } from './MainRoutes/Routes';
 
-      },
-      
-      {
-        path:"/Blogs",
-        element:<Blog></Blog>,
-        loader:()=>fetch('https://dev.to/api/articles?per_page=20?top=7')
-      },
-      {
-        path:`/blog/:id`,
-        element:<BlogDetails></BlogDetails>,
-        loader:({params})=>fetch(`https://dev.to/api/articles/${params.id}`),
-        children:[
-          {
-            index:true,
-            element:<Content></Content>
-          },
-          {
-            path:"author",
-            element:<Author></Author>
-            
-          }
-        ]
-        
-       
-      },
-      {
-        path:"/BookMarks",
-        element:<BookMarks></BookMarks>
-      }
-    ]
-  },
-]);
+
+
 
 createRoot(document.getElementById('root')).render(
  
