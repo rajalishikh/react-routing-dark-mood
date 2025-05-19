1ef53e5061d6f39bc7afb43a8db746c8bd8f72cd
rajalishikhe <nahidujjamanraj@gmail.com>
2025-05-19 09:28:04 +0600

    Now I am set the different path in my website like Author and content

--- src/Componets/Content/Content.jsx
+++ src/Componets/Content/Content.jsx
@@ -1,8 +1,34 @@
+import { useLoaderData } from "react-router-dom";
 
 const Content = () => {
+    const loaderData=useLoaderData()
+    console.log(loaderData)
+    const {cover_image,created_at,description,title,id}=loaderData
+
     return (
         <div>
-            <h1>I am a content </h1>
+            <div className="w-full p-4 shadow-md dark:bg-gray-50 dark:text-gray-800">
+	<div className="flex justify-between pb-4 border-bottom">
+		<div className="flex items-center">
+			<a rel="noopener noreferrer" href="#" className="mb-0 capitalize dark:text-gray-800">Photography</a>
+		</div>
+		<a rel="noopener noreferrer" href="#">See All</a>
+	</div>
+	<div className="space-y-4">
+		<div className="space-y-2">
+			<img src={cover_image} alt="" className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
+			<div className="flex items-center text-xs">
+				<span>6 min ago</span>
+			</div>
+		</div>
+		<div className="space-y-2">
+			<a rel="noopener noreferrer" href="#" className="block">
+				<h3 className="text-xl font-semibold dark:text-violet-600">{title}</h3>
+			</a>
+			<p className="leading-snug dark:text-gray-600">{description}</p>
+		</div>
+	</div>
+</div>
             
         </div>
     );
