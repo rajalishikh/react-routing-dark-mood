d1752d4489a9f4e1423908680865d08dc539a792
rajalishikhe <nahidujjamanraj@gmail.com>
2025-05-30 17:56:40 +0600

    Now I am show it in my bookmarks section

--- src/Componets/BookMarks/BookMarks.jsx
+++ src/Componets/BookMarks/BookMarks.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import Card from '../Card/Card';
+import { getBlogs } from '../Utility/Utility';
 
 const BookMarks = () => {
+    const [bookMarks,setBookMarks]=useState([])
+    useEffect(()=>{
+        const bringLocalStorageData=getBlogs()
+        setBookMarks(bringLocalStorageData)
+    },[])
     return (
-        <div>
-            <h2>welcome the bookmarks </h2>
-            
+       <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 px-6 mt-5">
+            {
+            bookMarks.map(blog=><Card blog={blog}></Card>)
+            }
+
         </div>
+		
     );
 };
 
