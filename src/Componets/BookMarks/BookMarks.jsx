fcc2e0ff0e24de3ecde5a4d771932622bf0e676a
rajalishikhe <nahidujjamanraj@gmail.com>
2025-06-01 08:37:07 +0600

    Now I am add a functionality which can remove a blog in a my local storage

--- src/Componets/BookMarks/BookMarks.jsx
+++ src/Componets/BookMarks/BookMarks.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Card from '../Card/Card';
-import { getBlogs } from '../Utility/Utility';
+import { deleteBlogs, getBlogs } from '../Utility/Utility';
 
 const BookMarks = () => {
     const [bookMarks,setBookMarks]=useState([])
@@ -8,11 +8,21 @@ const BookMarks = () => {
         const bringLocalStorageData=getBlogs()
         setBookMarks(bringLocalStorageData)
     },[])
+    const HandleDeleteBlogs=(id)=>{
+        deleteBlogs(id)
+         const bringLocalStorageData=getBlogs()
+        setBookMarks(bringLocalStorageData)
+
+
+    }
     return (
        <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 px-6 mt-5">
             {
-            bookMarks.map(blog=><Card blog={blog}></Card>)
+            bookMarks.map(blog=><Card blog={blog}  deletable={true} HandleDeleteBlogs={HandleDeleteBlogs}></Card>)
+            
+
             }
+            
 
         </div>
 		
--- src/Componets/Card/Card.jsx
+++ src/Componets/Card/Card.jsx
@@ -1,11 +1,12 @@
+import { MdDeleteSweep } from "react-icons/md";
 import { Link } from "react-router-dom";
 import img from "../../image/404 (1).jpg";
-const Card = ({blog}) => {
+const Card = ({blog,deletable,HandleDeleteBlogs}) => {
     console.log(blog)
     const{cover_image,created_at,description,title,id}=blog
     return (
         
-            <div className="transition hover:scale-105 border border-primary hover:border-pink-600 p-2 ">
+            <div className="transition hover:scale-105 border border-primary hover:border-pink-600 p-2 relative ">
 			<Link to={`/blog/${id}`} rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
 				<img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={cover_image || img } />
 				<div className="p-6 space-y-2">
@@ -14,6 +15,12 @@ const Card = ({blog}) => {
 					<p>{description}</p>
 				</div>
 			</Link>
+			{
+				deletable && <div onClick={()=>HandleDeleteBlogs(id)} className="absolute top-0 right-0 p-2 bg-red-600 ml-2 mb-2 rounded-xl hover:scale-105 hover:bg-red-800">
+                <MdDeleteSweep size={20} />
+            </div>
+			}
+			
 			
 		
             
